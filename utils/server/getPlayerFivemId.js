a3d7d352637b822402ffaed5593a0663ad54c1aa
David Peralta <peraltadavidr@outlook.com>
2020-09-17 21:11:17 +0800

    Add getPlayerFivemId util

---
+++ utils/server/getPlayerFivemId.js
@@ -0,0 +1,21 @@
+const getPlayerFivemId = async (playerSource, type = 'discord') => {
+  let discordIdentifier = null;
+
+  const playerIdentifierCount = GetNumPlayerIdentifiers(playerSource);
+
+  // loop all player identifiers
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < playerIdentifierCount; i++) {
+    const playerIdentifier = GetPlayerIdentifier(playerSource, i);
+    // Get discord identifier
+    if (playerIdentifier.includes(`${type}:`)) {
+      const [, identifier] = playerIdentifier.split(':');
+
+      discordIdentifier = identifier;
+    }
+  }
+
+  return discordIdentifier;
+};
+
+export default getPlayerFivemId;
---
+++ utils/server/index.js
@@ -0,0 +1,6 @@
+import getPlayerFivemId from './getPlayerFivemId';
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  getPlayerFivemId,
+};
