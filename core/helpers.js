561fc1039b45c2f6db83dbdc18e2115eb83816dd
David Peralta <peraltadavidr@outlook.com>
2020-09-23 12:34:04 +0800

    add: has & apply on reactive helper

--- core/helpers.js
+++ core/helpers.js
@@ -1,24 +1,42 @@
-const reactive = (object, handler) => {
-  const { get, set } = handler || {};
+const reactive = (object = Object, handler = Object) => {
+  const {
+    get, set, has, apply,
+  } = handler || {};
 
   if (typeof object !== 'object') throw new Error('invalid object type');
   if (get && typeof get !== 'function') throw new Error('getter must be a function');
   if (set && typeof set !== 'function') throw new Error('setter must be a function');
+  if (has && typeof has !== 'function') throw new Error('has must be a function');
+  if (apply && typeof apply !== 'function') throw new Error('apply must be a function');
 
   const proxyHandler = {
     get: (target, key) => {
-      if (key === 'isProxy') { return true; }
       const prop = target[key];
-      if (typeof prop === 'undefined') { return undefined; }
-      if (!prop.isProxy && typeof prop === 'object') { target[key] = new Proxy(prop, proxyHandler); }
 
-      if (get) get(target, key);
+      if (typeof prop === 'undefined') { return undefined; }
+      if (typeof prop === 'object') { target[key] = new Proxy(prop, proxyHandler); }
+
+      if (get) return get(target, key);
+
       return target[key];
     },
+
     set: (target, key, value) => {
-      if (set) set(target, key, value);
+      if (set) return set(target, key, value);
+
       target[key] = value;
+
       return true;
+    },
+
+    has: (target, key) => {
+      if (has) return has(target, key);
+      return key in target;
+    },
+
+    apply: (target, thisArg, args) => {
+      if (apply) return apply(target, thisArg, args);
+      return target.apply(thisArg, args);
     },
   };
 
